9b005c8e310adb587e08fd0ccee8ad2f6616228d
Sven Michael Klose <pixel@copei.de>
2014-02-05 00:51:44 +0100

    Have a bonus item.

--- gfx.asm
+++ gfx.asm
@@ -1,5 +1,6 @@
 gfx:
 ship:
+
     .byte %10110000
     .byte %11001000
     .byte %00110100
@@ -8,15 +9,7 @@ ship:
     .byte %01100111
     .byte %10111100
     .byte %11011010
-bullet:
-    .byte %00110000
-    .byte %01100100
-    .byte %11101100
-    .byte %11101100
-    .byte %01100100
-    .byte %00110000
-    .byte %00000000
-    .byte %00000000
+
 laser:
     .byte %00000000
     .byte %00000000
@@ -26,6 +19,7 @@ laser:
     .byte %11101110
     .byte %00010001
     .byte %00000000
+
 laser_up:
     .byte %00000111
     .byte %00001111
@@ -35,6 +29,7 @@ laser_up:
     .byte %11111000
     .byte %01110000
     .byte %10100000
+
 laser_down:
     .byte %10100000
     .byte %01010000
@@ -44,3 +39,23 @@ laser_down:
     .byte %00011111
     .byte %00001111
     .byte %00000111
+
+bullet:
+    .byte %00000000
+    .byte %00110000
+    .byte %01100100
+    .byte %11101100
+    .byte %11101100
+    .byte %01100100
+    .byte %00110000
+    .byte %00000000
+
+bonus:
+    .byte %11111111
+    .byte %10000001
+    .byte %10000001
+    .byte %10000001
+    .byte %10000001
+    .byte %10000001
+    .byte %10000001
+    .byte %11111111
