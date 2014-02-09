3d196d2539d3a646cc3bd977853b3eab3b0695ef
Sven Michael Klose <pixel@copei.de>
2014-02-09 06:57:31 +0100

    Big towers.

--- background.asm
+++ background.asm
@@ -5,6 +5,14 @@ scrbricks_i:.byte 0, 1
             .byte 2, 3
             .byte 2, 3
             .byte 2, 3
+            .byte 2, 3
+            .byte 2, 3
+            .byte 2, 3
+            .byte 2, 3
+            .byte 2, 3
+            .byte 2, 3
+            .byte 2, 3
+            .byte 2, 3
             .byte 4, 5
             .byte $ff
 scrbricks_x:.byte 22, 28
@@ -14,8 +22,24 @@ scrbricks_x:.byte 22, 28
             .byte 22, 28
             .byte 22, 28
             .byte 22, 28
+            .byte 22, 28
+            .byte 22, 28
+            .byte 22, 28
+            .byte 22, 28
+            .byte 22, 28
+            .byte 22, 28
+            .byte 22, 28
+            .byte 22, 28
             .byte 28, 47
-scrbricks_y:.byte 15, 15
+scrbricks_y:.byte 7, 7
+            .byte 8, 8
+            .byte 9, 9
+            .byte 10, 10
+            .byte 11, 11
+            .byte 12, 12
+            .byte 13, 13
+            .byte 14, 14
+            .byte 15, 15
             .byte 16, 16
             .byte 17, 17
             .byte 18, 18
