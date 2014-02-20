a36726feee75c5e100aa2b1aabfffd94b150673f
Sven Michael Klose <pixel@copei.de>
2014-02-20 20:53:24 +0100

    Stars with varying velocities and colours.

--- level-stars.asm
+++ level-stars.asm
@@ -8,6 +8,9 @@ add_star:
     lda random
     and #%11111000
     sta star_init+1
+    jsr update_random
+    lda random
+    sta star_init+7
     ldy #star_init-sprite_inits
     jsr add_sprite
     jsr update_random
