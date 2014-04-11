8ef994c601addc56bccd68b34877bb9dcf3279e6
Sven Michael Klose <pixel@copei.de>
2014-04-11 16:42:16 +0200

    Oops. Missing file.

---
+++ includes3k.asm
@@ -0,0 +1,40 @@
+#define M3K
+
+#include "vic.defs.asm"
+#include "game.defs.asm"
+#include "zeropage.asm"
+
+#include "basic-loader.asm"
+
+#include "init.asm"
+#include "intro.asm"
+
+#include "stackmem-start.asm"
+#include "random.asm"
+#include "score.asm"
+#include "stackmem-end.asm"
+
+#include "lowmem-start.asm"
+#include "blitter.asm"
+#include "chars.asm"
+#include "screen.asm"
+#include "math.asm"
+#include "level-stars.asm"
+#include "level-scout.asm"
+#include "tiles.asm"
+#include "bits.asm"
+#include "lowmem-end.asm"
+
+#include "init-end.asm"
+
+#include "sprites.asm"
+#include "controllers.asm"
+#include "main.asm"
+#include "level.asm"
+#include "level-bullet.asm"
+#include "level-sniper.asm"
+#include "foreground.asm"
+#include "gfx-sprites.asm"
+#include "gfx-tiles.asm"
+
+realend:
