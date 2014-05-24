564e9ae05f11577b24c247b7dfad534ffb24bfe5
Sven Michael Klose <pixel@copei.de>
2014-05-24 12:41:24 +0200

    Take low screen addresses from ROM.

--- screen.asm
+++ screen.asm
@@ -1,6 +1,6 @@
 scraddr:
     ldy scry
-    lda line_offsets_l,y
+    lda $edfd,y
     sta scr
     lda #>screen
     ora line_offsets_h,y
@@ -10,7 +10,7 @@ scraddr:
 
 scrcoladdr:
     ldy scry
-    lda line_offsets_l,y
+    lda $edfd,y
     sta scr
     sta col
     lda #>screen
@@ -22,7 +22,5 @@ scrcoladdr:
     ldy scrx
     rts
 
-line_offsets_l:
-    .byte 0, 22*1, 22*2, 22*3, 22*4, 22*5, 22*6, 22*7, 22*8, 22*9, 22*10, 22*11, <22*12, <22*13, <22*14, <22*15, <22*16, <22*17, <22*18, <22*19, <22*20, <22*21, <22*22
 line_offsets_h:
     .byte >0, >22*1, >22*2, >22*3, >22*4, >22*5, >22*6, >22*7, >22*8, >22*9, >22*10, >22*11, >22*12, >22*13, >22*14, >22*15, >22*16, >22*17, >22*18, >22*19, >22*20, >22*21, >22*22
