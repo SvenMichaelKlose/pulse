a103340648dd72c833457e2456cea0db206ae81b
Sven Michael Klose <pixel@copei.de>
2014-06-05 15:38:00 +0200

    A faster way to calculate line addresses without the high byte address table.

--- screen.asm
+++ screen.asm
@@ -1,26 +1,29 @@
+screen_h = >screen
+
+; Calculate line address in screen memory.
 scraddr:
     ldy scry
-    lda $edfd,y
+    lda $edfd,y         ; Get low line address.
     sta scr
-    lda #>screen
-    ora line_offsets_h,y
+    cpy #12             ; Set carry flag if above line 11.
+    lda #screen_h/2     ; Take screen page shifted 1 to the right...
+    rol                 ; ... and roll in carry flag to add it.
     sta scr+1
     ldy scrx
     rts
 
+; Calculate line address in screen and colour memory.
 scrcoladdr:
     ldy scry
     lda $edfd,y
     sta scr
     sta col
-    lda #>screen
-    ora line_offsets_h,y
+    cpy #12
+    lda #screen_h/2
+    rol
     sta scr+1
-    lda #>colors
-    ora line_offsets_h,y
+    and #1
+    ora #>colors
     sta col+1
     ldy scrx
     rts
-
-line_offsets_h:
-    .byte >0, >22*1, >22*2, >22*3, >22*4, >22*5, >22*6, >22*7, >22*8, >22*9, >22*10, >22*11, >22*12, >22*13, >22*14, >22*15, >22*16, >22*17, >22*18, >22*19, >22*20, >22*21, >22*22
