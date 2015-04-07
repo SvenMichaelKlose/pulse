a2d04a151bbc2ba49469bef51dae09f4fe4f9eb4
Sven Michael Klose <pixel@copei.de>
2015-04-07 19:31:47 +0200

    Oh dear! Forgot to add this. m(

---
+++ make.lisp
@@ -0,0 +1,41 @@
+(defvar *loader-start* nil)
+
+(apply #'assemble-files "pulse.prg"
+      '("vic.defs.asm"
+        "game.defs.asm"
+        "zeropage.asm"
+
+        "basic-loader.asm"
+
+        "init.asm"
+          "intro.asm"
+
+          "stackmem-start.asm"
+            "random.asm"
+            "score.asm"
+          "stackmem-end.asm"
+
+          "lowmem-start.asm"
+            "blitter.asm"
+            "chars.asm"
+            "screen.asm"
+            "math.asm"
+            "level-bullet.asm"
+            "level-scout.asm"
+            "level-sniper.asm"
+          "lowmem-end.asm"
+        "init-end.asm"
+
+        "bits.asm"
+        "sprites.asm"
+        "controllers.asm"
+        "main.asm"
+        "foreground.asm"
+        "level.asm"
+        "gfx-sprites.asm"
+        "gfx-tiles.asm"
+        "tiles.asm"
+        "level-data.asm"
+        "level-patterns.asm"))
+(make-vice-commands "vice.txt")
+(quit)
