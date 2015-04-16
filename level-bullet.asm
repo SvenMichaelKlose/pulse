a20895c7c8f2aadc71f0c8774d588896cf5c7ff9
Sven Michael Klose <pixel@copei.de>
2015-04-16 17:33:42 +0200

    Knocked off a couple of bytes with help of a lookup table to swap axis.

--- level-bullet.asm
+++ level-bullet.asm
@@ -2,6 +2,12 @@ step_y = 16
 inc_x  = 8
 inc_y  = 4
 
+swapped_axis_flags:
+    @(+ deadly step_y)
+    @(+ deadly step_y inc_x)
+    @(+ deadly step_y inc_y)
+    @(+ deadly step_y inc_x inc_y)
+
 add_bullet:
     inc sound_foreground
 add_bullet_no_sound:
@@ -21,43 +27,39 @@ add_bullet_no_sound:
     sbc sprites_x,x
     jsr abs
     sta distance_x
+
     lda @(+ sprites_y 15)    ; Get Y distance to player.
     sec
     sbc sprites_y,x
     jsr abs
     sta distance_y
 
-    cmp distance_x           ; Get incremented axis.
-    bcc +n5
+    cmp distance_x
+    bcc +n5                 ; Will step along the Y axis.
 
-    lda tmp                  ; Swap axis.
-    tay
-    and #inc_x
-    asl
-    sta tmp
-    tya
-    and #inc_y
+    lda tmp                 ; Swap direction flags.
+    and #@(+ inc_x inc_y)
     lsr
-    ora tmp
-    ora #@(+ deadly step_y)
+    lsr
+    tay
+    lda swapped_axis_flags,y
     sta tmp
+
     ldy distance_x
     lda distance_y
     sta distance_x
     sty distance_y
 
-n5: lda distance_x
+n5: 
+l1: asl distance_x          ; Scale fraction up to byte.
     beq +d1
-    lda distance_y
-    beq +d1
-
-l1: asl distance_x             ; Scale fraction up to byte.
     bcs +d1
     asl distance_y
+    beq +d1
     bcc -l1
 
 d1: lda distance_y
-    and #%11110000      ; Save 4 bits fraction.
+    and #%11110000          ; Save 4 bits fraction.
     sta @(+ bullet_init 7)
     lda sprites_x,x
     sta bullet_init
