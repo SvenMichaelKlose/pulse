d466b5a09986549f96f6c063ff5a931d3cca879b
Sven Michael Klose <pixel@copei.de>
2015-04-17 21:37:03 +0200

    Put file list in different file for the tape release.

--- make.lisp
+++ make.lisp
@@ -1,42 +1,7 @@
+(load "files.lisp")
+
 (defvar *loader-start* nil)
 
-(apply #'assemble-files "pulse.prg"
-      '("vic.defs.asm"
-        "game.defs.asm"
-        "zeropage.asm"
-
-        "basic-loader.asm"
-
-        "init.asm"
-          "intro.asm"
-
-          "stackmem-start.asm"
-            "random.asm"
-            "score.asm"
-          "stackmem-end.asm"
-
-          "lowmem-start.asm"
-            "blitter.asm"
-            "chars.asm"
-            "screen.asm"
-            "math.asm"
-            "level-bullet.asm"
-            "level-scout.asm"
-            "level-sniper.asm"
-          "lowmem-end.asm"
-        "init-end.asm"
-
-        "bits.asm"
-        "gfx-sprites.asm"
-        "gfx-tiles.asm"
-        "tiles.asm"
-        "level-data.asm"
-        "level-patterns.asm"
-        "sprites.asm"
-        "controllers.asm"
-        "grenade.asm"
-        "main.asm"
-        "foreground.asm"
-        "level.asm"))
+(apply #'assemble-files "pulse.prg" (. "basic-loader.asm" +pulse-files+))
 (make-vice-commands "vice.txt")
 (quit)
