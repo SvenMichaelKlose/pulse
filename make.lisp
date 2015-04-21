5c874aedf1a08712797741a8fe54f3c30000babe
Sven Michael Klose <pixel@copei.de>
2015-04-21 10:48:29 +0200

    Moved game sources into own directory.

--- make.lisp
+++ make.lisp
@@ -1,8 +1,8 @@
 (load "files.lisp")
 (load "story.lisp")
 
-(defvar *loader-start* nil)
-
-(apply #'assemble-files "pulse.prg" (. "basic-loader.asm" +pulse-files+))
+(apply #'assemble-files
+       "pulse.prg"
+       (@ [+ "game/" _] (. "basic-loader.asm" +pulse-files+)))
 (make-vice-commands "vice.txt")
 (quit)
