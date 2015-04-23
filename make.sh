cc6c9f9422e3747e0cc3a93146c88d8624154c17
Sven Michael Klose <pixel@copei.de>
2015-04-23 12:50:48 +0200

    Also convert audio files.

--- make.sh
+++ make.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+rm -rf obj
+mkdir obj
+cd spinoffs ; ./make-audio.sh ; cd -
 sbcl --core bender/bender make.lisp
 #xa -M includes.asm -o pulse.prg -l labels.lst
 #xa -M includes.asm -DTAPE_RELEASE=1 -o pulse.bin
