4026addf1f8692277e648aedec105137fcd306b6
Sven Michael Klose <pixel@copei.de>
2015-04-24 13:41:50 +0200

    Cleaned up.

--- make.lisp
+++ make.lisp
@@ -5,9 +5,16 @@
 (defvar *pulse-long* #x30)
 (defvar *tape-pulse* (* 8 (+ *pulse-short* (half (- *pulse-long* *pulse-short*)))))
 
+(load "tape-loader/bin2pottap.lisp")
 (load "spinoffs/wav2pwm.lisp")
 (load "game/files.lisp")
 (load "game/story.lisp")
+
+(defun make-tape-wav (in-file out-file)
+  (format t "Making tape WAV '~A' of '~A'...~%" out-file in-file)
+  (with-input-file in in-file
+    (with-output-file out out-file
+      (tap2wav in out))))
 
 (defun make-game (tape?)
   (apply #'assemble-files
@@ -55,71 +62,61 @@
         "spinoffs/tape_audio_player.asm"))
   (make-vice-commands "obj/ohne_dich.bin.vice.txt"))
 
-(make-game nil)
-(make-vice-commands "obj/pulse.vice.txt")
-(make-game t)
-(make-vice-commands "obj/game.vice.txt")
-(defvar *game-start* (get-label 'main))
+(defvar *game-start* nil)
+(defvar loaded_tape_loader nil)
+(defvar waiter nil)
+(defvar waiter_end nil)
+(defvar run nil)
 
-(make-loader-bin)
-(defvar loaded_tape_loader (get-label 'loaded_tape_loader))
-(defvar waiter (get-label 'waiter))
-(defvar waiter_end (get-label 'waiter_end))
-(defvar run (get-label 'run))
-(make-loader-prg)
+(defun make-all-games ()
+  (make-game nil)
+  (make-vice-commands "obj/pulse.vice.txt")
+  (make-game t)
+  (make-vice-commands "obj/game.vice.txt")
+  (= *game-start* (get-label 'main))
 
-(defvar ohne_dich 0)
-(make-ohne-dich-prg)
-(defvar text (get-label 'text))
-(make-ohne-dich-bin)
-(= ohne_dich (get-label 'ohne_dich))
-(make-ohne-dich-prg)
+  (make-loader-bin)
+  (= loaded_tape_loader (get-label 'loaded_tape_loader))
+  (= waiter (get-label 'waiter))
+  (= waiter_end (get-label 'waiter_end))
+  (= run (get-label 'run))
+  (make-loader-prg)
 
-(defun bin2pottap-byte (q i)
-  (when (< i 0)
-    (= i (+ 256 i)))
-   (dotimes (j 8)
-     (enqueue q (code-char (? (zero? (bit-and i 1))
-                              *pulse-short*
-                              *pulse-long*)))
-     (= i (>> i 1))))
+  (with-output-file o "compiled/pulse.tap"
+    (write-tap o
+        (+ (bin2cbmtap (cddr (string-list (fetch-file "obj/loader.prg")))
+                       (+ "PULSE           "
+                          (fetch-file "obj/loader.bin"))
+                       :start #x1001)
+           (bin2pottap (string-list (fetch-file "obj/game.bin"))))))
 
-(defun bin2pottap (x)
-  (with-queue q
-    (enqueue q (code-char #x00))
-    (enqueue q (code-char #x00))
-    (enqueue q (code-char #x00))
-    (enqueue q (code-char #x04))
-    (enqueue q (code-char #x30))
-    (dolist (i x (list-string (queue-list q)))
-      (bin2pottap-byte q i))))
+  (when +make-wav?+
+    (make-tape-wav "compiled/pulse.tap" "compiled/pulse.tape.wav")))
 
-(with-output-file o "compiled/pulse.tap"
-  (write-tap o
-      (+ (bin2cbmtap (cddr (string-list (fetch-file "obj/loader.prg")))
-                     (+ "PULSE           "
-                        (fetch-file "obj/loader.bin"))
-                     :start #x1001)
-         (bin2pottap (string-list (fetch-file "obj/game.bin"))))))
+(defvar ohne_dich nil)
+(defvar text nil)
 
-(with-output-file o "compiled/ohne_dich.tap"
-  (write-tap o
-      (bin2cbmtap (cddr (string-list (fetch-file "obj/ohne_dich.prg")))
-                  (+ "OHNE DICH       "
-                     (fetch-file "obj/ohne_dich.bin"))
-                  :start #x1001))
-  (wav2pwm o "obj/ohne_dich_downsampled.wav"))
+(defun make-ohne-dich ()
+  (= ohne_dich 0)
+  (make-ohne-dich-prg)
+  (= text (get-label 'text))
+  (make-ohne-dich-bin)
+  (= ohne_dich (get-label 'ohne_dich))
+  (make-ohne-dich-prg)
 
-(defun make-tape-wav (in-file out-file)
-  (format t "Making tape WAV '~A' of '~A'...~%" out-file in-file)
-  (with-input-file in in-file
-    (with-output-file out out-file
-      (tap2wav in out))))
+  (with-output-file o "compiled/ohne_dich.tap"
+    (write-tap o
+        (bin2cbmtap (cddr (string-list (fetch-file "obj/ohne_dich.prg")))
+                    (+ "OHNE DICH       "
+                       (fetch-file "obj/ohne_dich.bin"))
+                    :start #x1001))
+    (wav2pwm o "obj/ohne_dich_downsampled.wav"))
 
-(when +make-wav?+
-  (make-tape-wav "compiled/pulse.tap" "compiled/pulse.tape.wav")
-  (make-tape-wav "compiled/ohne_dich.tap" "compiled/ohne_dich.tape.wav"))
+  (when +make-wav?+
+    (make-tape-wav "compiled/ohne_dich.tap" "compiled/ohne_dich.tape.wav")))
 
+(make-all-games)
+(make-ohne-dich)
 (print-pwm-info)
 
 (format t "Done making 'Pulse'. See directory 'compiled/'.~%")
---
+++ tape-loader/bin2pottap.lisp
@@ -0,0 +1,18 @@
+(defun bin2pottap-byte (q i)
+  (when (< i 0)
+    (= i (+ 256 i)))
+   (dotimes (j 8)
+     (enqueue q (code-char (? (zero? (bit-and i 1))
+                              *pulse-short*
+                              *pulse-long*)))
+     (= i (>> i 1))))
+
+(defun bin2pottap (x)
+  (with-queue q
+    (enqueue q (code-char #x00))
+    (enqueue q (code-char #x00))
+    (enqueue q (code-char #x00))
+    (enqueue q (code-char #x04))
+    (enqueue q (code-char #x30))
+    (dolist (i x (list-string (queue-list q)))
+      (bin2pottap-byte q i))))
