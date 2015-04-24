e1ab37e6195bf2b5b4feafaf9e6e39a365d19f3e
Sven Michael Klose <pixel@copei.de>
2015-04-24 03:58:49 +0200

    Added comments and knocked off two bytes.

--- spinoffs/tape_audio_player.asm
+++ spinoffs/tape_audio_player.asm
@@ -1,33 +1,33 @@
-timer = @(- (* 8 audio_longest_pulse) (half (+ 2 2 3 4)))
+average_loop_cycles = @(half (+ 4 2 2 3))
+timer = @(- (* 8 audio_longest_pulse) average_loop_cycles)
 
 tape_audio_player:
-    lda via_peripheral_ctrl ; Start motor.
+    ; Start tape motor.
+    lda via_peripheral_ctrl
     and #$fd
     sta via_peripheral_ctrl
 
+    ; Initialize VIA2 T1 timing and one-shot mode.
     lda #0
     sta $912b
     lda #<timer
     sta $9124
-    lda #>timer
-    tay
-    sta $9125
+    ldy #>timer
 
-f:  lda $9121
-l:  lda $912d
-    lsr
-    lsr
-    bcc -l
+    ; Play.
+f:  lda $9121   ; Reset the VIA2 CA1 status bit.
+l:  lda $912d   ; (4) Reset the VIA2 CA1 status bit.
+    lsr         ; (2) Shift to test bit 2.
+    lsr         ; (2)
+    bcc -l      ; (3) Nothing happened yet. Try again…
 
-    lda $9124
-    sty $9125
+    lda $9124   ; Read the timer's low byte which is your sample.
+    sty $9125   ; Write high byte to restart the timer.
+    lsr         ; Reduce sample from 7 to 4 bits.
     lsr
     lsr
-    lsr
-    sta vicreg_auxcol_volume
-    sta vicreg_screencol_reverse_border
-;    sta $1e00,x
-;    inx
+    sta vicreg_auxcol_volume    ; Play it!
+    sta vicreg_screencol_reverse_border ; Something for the eye.
 
     jmp -f
 
