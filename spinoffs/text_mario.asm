af06eefe39f7756432d72c82eb1d14d747ec2c95
Sven Michael Klose <pixel@copei.de>
2015-04-26 19:48:24 +0200

    Missing file added.

---
+++ spinoffs/text_mario.asm
@@ -0,0 +1,14 @@
+text:
+    $93 18 5
+    "NOW PLAYING FROM TAPE:" 13
+    13
+    13
+    " " 18 5"\"SUPER MARIO THEME\"" 13
+    13
+    13
+    "REWIND OR FORWARD THE" 13
+    "TAPE IF YOU SO DESIRE." 13
+    " OUTPUT IS " @(string-list (princ (integer (pwm-pulse-rate *tv*)) nil)) "HZ AT" 13
+    "      FOUR BITS." 13
+    0
+
