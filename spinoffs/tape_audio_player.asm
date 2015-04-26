875025dcfca4db13b776bd7e313c5476ebb353e6
Sven Michael Klose <pixel@copei.de>
2015-04-26 19:46:58 +0200

    Cleaned up.

--- spinoffs/tape_audio_player.asm
+++ spinoffs/tape_audio_player.asm
@@ -1,6 +1,6 @@
-current_low = 4
-average = 5
-tleft = 7
+current_low = 0
+average = 1
+tleft = 3
 
 average_loop_cycles = @(half (+ 4 2 2 3))
 sure_delay = 7
@@ -13,11 +13,8 @@ tape_audio_player:
     sta $911c
 
     ; Initialize VIA2 timer 1.
-    ldx #0      ; one-shot mode
-    stx $912b
-a:  lda #0
-    sta average
-    sta @(++ average)
+    lda #0
+    sta $912b   ; one-shot mode
     lda #<timer
     sta current_low
     ldy #>timer
@@ -45,25 +42,25 @@ l:  lda $912d   ; (4) Read the VIA2 CA1 status bit.
     sta average
     bcc +n
     inc @(++ average)
-    jmp -f
+    bne -f
 
-    ; Continue playing if less than 128 samples have been summed up.
 n:  dec tleft
     bne -f
 
-    ; Correct time of average pulse length doesn't match our desired value.
-    lda @(++ average)   ; average / 256
-    cmp #$29            ; 41… why?
-    beq +j
+    ; Correct time if average pulse length doesn't match our desired value.
+s:  lda @(++ average)   ; average / 256
+    tax
+    cmp #$29            ; 41… why? Should be $40.
+    beq +j              ; It's already what we want.
     bcc +n
     dec current_low
-    jmp +d
+    bne +d
 n:  inc current_low
 d:  lda current_low
     sta $9124
 
     ; Divide average by 128 and restart summing up samples.
-j:  lda @(++ average)
+j:  txa
     asl
     sta average
     lda #0
@@ -71,6 +68,6 @@ j:  lda @(++ average)
     sta @(++ average)
     lda #128
     sta tleft
-    jmp -f      ; (4)
+    bne -f      ; (4)
 
 ;xlat: @(amplitude-conversions)
