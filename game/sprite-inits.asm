a8f4348ccfeb4940cd2f0b5088d0511f94287011
Sven Michael Klose <pixel@copei.de>
2015-05-11 04:52:52 +0200

    Set all unused values to 0.

--- game/sprite-inits.asm
+++ game/sprite-inits.asm
@@ -7,29 +7,29 @@ player_init:
     0 80 0 cyan <ship
     <player_fun >player_fun 0
 laser_init:
-    18 80 0 @(+ white multicolor) <laser
+    0 0 0 @(+ white multicolor) <laser
     <laser_fun >laser_fun 0
 laser_up_init:
-    18 80 0 yellow <laser_up
+    0 0 0 yellow <laser_up
     <laser_up_fun >laser_up_fun 0
 laser_down_init:
-    18 80 0 yellow <laser_down
+    0 0 0 yellow <laser_down
     <laser_down_fun >laser_down_fun 0
 bullet_init:
-    176 89 @(+ deadly 0) @(+ yellow multicolor) <bullet
+    0 0 @(+ deadly 0) @(+ yellow multicolor) <bullet
     <bullet_fun >bullet_fun 0
 scout_init:
-    176 89 @(+ deadly 1) @(+ yellow multicolor) <scout
+    176 0 @(+ deadly 1) @(+ yellow multicolor) <scout
     <scout_fun >scout_fun 0
 sniper_init:
-    176 89 @(+ deadly 2) white <sniper
+    176 0 @(+ deadly 2) white <sniper
     <sniper_fun >sniper_fun 0
 bonus_init:
-    176 89 4 green <bonus
+    0 0 0 green <bonus
     <bonus_fun >bonus_fun 0
 star_init:
-    176 89 decorative white <star
+    0 0 decorative white <star
     <star_fun >star_fun 0
 explosion_init:
-    176 89 decorative yellow 0
+    0 0 decorative yellow 0
     <explosion_fun >explosion_fun 15
