f745056d8c160dd18b453637fd23130a050243ee
Sven Michael Klose <pixel@copei.de>
2015-05-12 12:26:44 +0200

    Uncommented cloning of repositories.

--- download-and-build.sh
+++ download-and-build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#git clone https://github.com/SvenMichaelKlose/tre
-#git clone https://github.com/SvenMichaelKlose/bender
-#git clone https://github.com/SvenMichaelKlose/pulse
+git clone https://github.com/SvenMichaelKlose/tre
+git clone https://github.com/SvenMichaelKlose/bender
+git clone https://github.com/SvenMichaelKlose/pulse
 cd tre; ./make.sh core; ./make.sh install; cd ..
 ln -s ../tre/environment bender/
 cd bender; ./make.sh; cd ..
