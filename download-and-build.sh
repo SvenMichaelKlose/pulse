423dccf450a541ba9208c1815e932763cbccdd2a
Sven Michael Klose <pixel@copei.de>
2015-05-30 23:42:22 +0200

    'nipkow' was missing.

--- download-and-build.sh
+++ download-and-build.sh
@@ -2,9 +2,11 @@
 
 git clone https://github.com/SvenMichaelKlose/tre
 git clone https://github.com/SvenMichaelKlose/bender
+git clone https://github.com/SvenMichaelKlose/nipkow
 git clone https://github.com/SvenMichaelKlose/pulse
 cd tre; ./make.sh core; ./make.sh install; cd ..
 ln -s ../tre/environment bender/
 cd bender; ./make.sh; cd ..
 ln -s ../bender pulse/
+ln -s ../nipkow pulse/
 cd pulse; ./make.sh; cd ..
