e62fc48bc4284f777adbdc278aaa41b4196f79ff
Sven Michael Klose <pixel@copei.de>
2015-12-22 12:45:44 +0000

    Center EYES logo.

--- eyes/main.asm
+++ eyes/main.asm
@@ -1,11 +1,13 @@
 screen_columns  = 22
 screen_rows  = 23
+xpos = 4
 
 loaded_intro:
 
     org $1a00
 
 intro_start:
+    ; Clear zero page.
     ldx #0
     txa
 l:  sta 0,x
@@ -20,6 +22,9 @@ l:  sta 0,x
     lda #@(+ reverse black) ; Screen and border color.
     sta $900f
     jsr clear_screen
+
+    inc $9000
+
 l:  lda $9004
     bne -l
     lda #150            ; Unblank screen.
@@ -38,7 +43,7 @@ l:  lda $9004
     sta @(++ s)
     lda #9
     sta scry
-l:  lda #11
+l:  lda #@(+ xpos 4)
     sta scrx
     jsr strout
     jsr inc_s
@@ -47,25 +52,25 @@ l:  lda #11
     bne -l
 
     lda #$ec
-    sta @(+ screen 7 (* 9 22))
+    sta @(+ screen xpos 0 (* 9 22))
     lda #$e2
-    sta @(+ screen 8 (* 9 22))
-    sta @(+ screen 9 (* 9 22))
+    sta @(+ screen xpos 1 (* 9 22))
+    sta @(+ screen xpos 2 (* 9 22))
     lda #$fb
-    sta @(+ screen 10 (* 9 22))
+    sta @(+ screen xpos 3 (* 9 22))
     lda #$61
-    sta @(+ screen 7 (* 10 22))
-    sta @(+ screen 7 (* 11 22))
+    sta @(+ screen xpos 0 (* 10 22))
+    sta @(+ screen xpos 0 (* 11 22))
     lda #$e1
-    sta @(+ screen 10 (* 10 22))
-    sta @(+ screen 10 (* 11 22))
+    sta @(+ screen xpos 3 (* 10 22))
+    sta @(+ screen xpos 3 (* 11 22))
     lda #$fc
-    sta @(+ screen 7 (* 12 22))
+    sta @(+ screen xpos 0 (* 12 22))
     lda #$62
-    sta @(+ screen 8 (* 12 22))
-    sta @(+ screen 9 (* 12 22))
+    sta @(+ screen xpos 1 (* 12 22))
+    sta @(+ screen xpos 2 (* 12 22))
     lda #$fe
-    sta @(+ screen 10 (* 12 22))
+    sta @(+ screen xpos 3 (* 12 22))
 
     jmp blink
 
@@ -86,16 +91,16 @@ txt_eyes:
 
 blink:
     lda #white
-    sta @(+ colors 11 (* 9 22))
+    sta @(+ colors xpos 4 (* 9 22))
     jsr laser
     lda #white
-    sta @(+ colors 11 (* 11 22))
+    sta @(+ colors xpos 4 (* 11 22))
     jsr laser
     lda #white
-    sta @(+ colors 11 (* 10 22))
+    sta @(+ colors xpos 4 (* 10 22))
     jsr laser
     lda #white
-    sta @(+ colors 11 (* 12 22))
+    sta @(+ colors xpos 4 (* 12 22))
     ldx #16
     jsr laser2
 
@@ -124,7 +129,7 @@ l:  ldx #1
 
 show_text:
     sta scry
-    lda #11
+    lda #@(+ xpos 4)
     sta scrx
     jsr scrcoladdr
     lda #white
@@ -144,18 +149,18 @@ laser2:
     jmp swait
 
 set_rectangle_color:
-    sta @(+ colors 7 (* 9 22))
-    sta @(+ colors 8 (* 9 22))
-    sta @(+ colors 9 (* 9 22))
-    sta @(+ colors 10 (* 9 22))
-    sta @(+ colors 7 (* 10 22))
-    sta @(+ colors 7 (* 11 22))
-    sta @(+ colors 10 (* 10 22))
-    sta @(+ colors 10 (* 11 22))
-    sta @(+ colors 7 (* 12 22))
-    sta @(+ colors 8 (* 12 22))
-    sta @(+ colors 9 (* 12 22))
-    sta @(+ colors 10 (* 12 22))
+    sta @(+ colors xpos 0 (* 9 22))
+    sta @(+ colors xpos 1 (* 9 22))
+    sta @(+ colors xpos 2 (* 9 22))
+    sta @(+ colors xpos 3 (* 9 22))
+    sta @(+ colors xpos 0 (* 10 22))
+    sta @(+ colors xpos 0 (* 11 22))
+    sta @(+ colors xpos 3 (* 10 22))
+    sta @(+ colors xpos 3 (* 11 22))
+    sta @(+ colors xpos 0 (* 12 22))
+    sta @(+ colors xpos 1 (* 12 22))
+    sta @(+ colors xpos 2 (* 12 22))
+    sta @(+ colors xpos 3 (* 12 22))
     rts
 
 swait:
@@ -176,4 +181,5 @@ done:
 
 init_3k:
     jsr clear_screen
+    dec $9000
     jmp $1002
