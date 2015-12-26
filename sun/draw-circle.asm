681941ddfb39eeaea5c7854369360f2493aae1ce
Sven Michael Klose <pixel@copei.de>
2015-12-26 07:25:57 +0000

    Removed unused code.

--- sun/draw-circle.asm
+++ sun/draw-circle.asm
@@ -1,21 +1,3 @@
-draw_circle:
-    lda #0
-    sta counter
-
-l:  lda counter
-    sta degrees
-    lda #@(half screen_columns)
-    sta xpos
-    lda #@(half screen_rows)
-    sta ypos
-    jsr point_on_circle
-
-    jsr draw_pixel
-
-    inc counter
-    bne -l
-    rts
-
 draw_random_point_on_circle:
     jsr random
     sta degrees
