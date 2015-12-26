716a3d7bae2c8fe7b872f1ea9c5222361ef42a3f
Sven Michael Klose <pixel@copei.de>
2015-12-26 07:07:21 +0000

    Boost audio.

--- message/main.asm
+++ message/main.asm
@@ -31,6 +31,22 @@ intro_message:
     ldx #$ff
     txs
 
+    ; Boost digital audio with distorted HF carrier.
+    lda #$0f
+    sta $900e
+    ldx #$7e
+    stx $900c
+    ldy #0
+l:  dey
+    bne -l
+    lda #$fe
+    stx $900c
+    stx $900c
+    sta $900c
+    sta $900c
+    stx $900c
+    sta $900c
+
     ldy #<loader_cfg_8k
     lda #>loader_cfg_8k
     jsr tape_loader_start
