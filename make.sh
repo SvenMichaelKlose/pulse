a51a3c55bce1b02b7a235c7831fbef97abf5e631
Sven Michael Klose <pixel@hugbox.org>
2023-03-13 01:32:46 +0100

    Double up heap.

--- make.sh
+++ make.sh
@@ -3,4 +3,4 @@
 rm -rf obj  # Don't uncomment this even if you have untreated OCD.
 mkdir -p obj
 mkdir -p compiled
-sbcl --dynamic-space-size 1024 --noinform --core bender/bender make.lisp
+sbcl --dynamic-space-size 2048 --noinform --core bender/bender make.lisp
